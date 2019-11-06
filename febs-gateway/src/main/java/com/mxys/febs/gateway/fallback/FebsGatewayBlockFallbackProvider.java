fd45fe54af49ae4ccf7dfe86a530d1a3de226d90
xpf0321 <18813004399@163.com>
2019-11-06 15:59:38 +0800

    重写第3版完成：创建了数据库表，完善了令牌获取流程，并在该流程中加入了图形验证码验证逻辑，同时也对验证码接口进行了限流处理。

---
+++ febs-gateway/src/main/java/com/mxys/febs/gateway/fallback/FebsGatewayBlockFallbackProvider.java
@@ -0,0 +1,23 @@
+package com.mxys.febs.gateway.fallback;
+
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.BlockResponse;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.ZuulBlockFallbackProvider;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.sun.deploy.security.BlockedException;
+import org.springframework.http.HttpStatus;
+
+public class FebsGatewayBlockFallbackProvider implements ZuulBlockFallbackProvider {
+    @Override
+    public String getRoute() {
+        return "*";
+    }
+
+    @Override
+    public BlockResponse fallbackResponse(String s, Throwable throwable) {
+        if(throwable instanceof BlockException){
+            return new BlockResponse(HttpStatus.TOO_MANY_REQUESTS.value(),"访问频率超限",s);
+        }else{
+            return new BlockResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),"系统异常",s);
+        }
+    }
+}
